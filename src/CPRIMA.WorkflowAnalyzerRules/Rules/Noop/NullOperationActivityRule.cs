986bc6ce7989fc34101aedfd739857c0423b17c8
Christian Prior-Mamulyan <cprior@gmail.com>
2025-03-19 16:44:42 +0100

    Implementing ShouldStopActivityRule; refactoring

--- src/CPRIMA.WorkflowAnalyzerRules/Rules/Noop/NullOperationActivityRule.cs
+++ src/CPRIMA.WorkflowAnalyzerRules/Rules/Noop/NullOperationActivityRule.cs
@@ -1,22 +1,32 @@
+using System.Diagnostics;
 using UiPath.Studio.Activities.Api;
 using UiPath.Studio.Activities.Api.Analyzer;
 using UiPath.Studio.Activities.Api.Analyzer.Rules;
 using UiPath.Studio.Analyzer.Models;
+using CPRIMA.WorkflowAnalyzerRules.LocalizationResources;
 
 namespace CPRIMA.WorkflowAnalyzerRules.Rules.Noop
 {
     public class NullOperationActivityRule : IRegisterAnalyzerConfiguration
     {
-        private const string RuleId = "ST-CPM-003";
+        private const string RuleId = "CPM_NOOP_003";
 
-        public void Initialize(IAnalyzerConfigurationService configService)
+        public Rule<IActivityModel> Get()
         {
-            var nullOpRule = new Rule<IActivityModel>("Null Activity Rule", RuleId, InspectActivity);
-            nullOpRule.DefaultErrorLevel = System.Diagnostics.TraceLevel.Info;
-            configService.AddRule<IActivityModel>(nullOpRule);
+            return new Rule<IActivityModel>(Strings.CPM_NOOP_003_Name, RuleId, InspectActivity)
+            {
+                RecommendationMessage = Strings.CPM_NOOP_003_Recommendation,
+                DefaultErrorLevel = TraceLevel.Info,
+                DocumentationLink = "https://github.com/rpapub/WatchfulAnvil"
+            };
         }
 
-        private InspectionResult InspectActivity(IActivityModel activity, Rule rule)
+        public void Initialize(IAnalyzerConfigurationService workflowAnalyzerConfigService)
+        {
+            workflowAnalyzerConfigService.AddRule<IActivityModel>(Get());
+        }
+
+        private InspectionResult InspectActivity(IActivityModel activity, Rule configuredRule)
         {
             return new InspectionResult { HasErrors = false };
         }
